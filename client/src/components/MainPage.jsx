b66898a50beb58cdfa3dbd828627f062fbb2a813
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-06-29 23:24:53 -0500

    refactored for blog post

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -2,35 +2,38 @@ import { useState, useRef } from "react";
 import { ThreeDots } from 'react-loader-spinner';
 
 function MainPage() {
-  const [position, setPosition] = useState("");
-  const [company, setCompany] = useState("");
-  const [skills, setSkills] = useState("");
-  const [companyValues, setCompanyValues] = useState("");
-  const [jobListing, setJobListing] = useState("");
-  const [coverLetter, setCoverLetter] = useState("");
+  const [topic, setTopic] = useState("");
+  const [audience, setAudience] = useState("");
+  const [goal, setGoal] = useState("");
+  const [takeaways, setTakeaways] = useState("");
+  const [angle, setAngle] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const [keywordHelp, setKeywordHelp] = useState("No");
+  const [format, setFormat] = useState("");
+  const [tone, setTone] = useState("");
+  const [sources, setSources] = useState("");
+  const [credibility, setCredibility] = useState("");
+  const [article, setArticle] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const coverLetterRef = useRef(null);
+  const articleRef = useRef(null);
 
-  const handleGenerateCoverLetter = async () => {
+  const handleGenerateArticle = async () => {
     const options = {
       method: "POST",
       body: JSON.stringify({
-        message:
-           "Create me a concise cover letter for the " +
-            position +
-            " at " +
-            company +
-            ". The cover letter should be in the following format:" +
-            "1. Salutation" +
-            "2. Introduction and interest in the position and company" +
-            "3. Summary of relevant skills and experiences: " +
-            skills +
-            "4. Explanation of how my values align with the company's values: " +
-            companyValues +
-            "5. Closing and contact information" +
-            "Here is the job listing for reference: " +
-            jobListing +
-            ". Please keep the letter professional, engaging, and no more than a few paragraphs long.",
+        message: `
+          Create me an article with the following details:
+          1. Topic: ${topic}
+          2. Target Audience: ${audience}
+          3. Goal: ${goal}
+          4. Key Takeaways: ${takeaways}
+          5. Unique Angle: ${angle}
+          6. Keywords: ${keywords ? keywords : "No specific keywords, need help with keyword research"}
+          7. Format: ${format}
+          8. Tone: ${tone}
+          9. Sources: ${sources ? "Yes, will provide links later" : "No"}
+          10. Crucial Information for Credibility: ${credibility ? "Yes, will provide details later" : "No"}
+        `,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -40,143 +43,152 @@ function MainPage() {
     setIsLoading(true);
 
     try {
-      const response = await fetch(
-        // "https://cvl-generator-a8eca88952f3.herokuapp.com/completions",  options );
-        // "https://cover-letter-akira-backend-b62c253075b0.herokuapp.com/completions",  options );
-        "http://localhost:3001/completions",  options );
-
-        const data = await response.json();
-      // console.log(data);
-      setCoverLetter(data.choices[0].message);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Failed to generate cover letter:", error);
-      setIsLoading(false);
-    }
-  };
-
-  const handleGenerateNewCoverLetter = async () => {
-    setIsLoading(true);
-
-    try {
-      const options = { // Define the 'options' variable here
-        method: "POST",
-        body: JSON.stringify({
-          message:
-           "Create me a concise cover letter for the " +
-            position +
-            " at " +
-            company +
-            ". The cover letter should be in the following format:" +
-            "1. Salutation" +
-            "2. Introduction and interest in the position and company" +
-            "3. Summary of relevant skills and experiences: " +
-            skills +
-            "4. Explanation of how my values align with the company's values: " +
-            companyValues +
-            "5. Closing and contact information" +
-            "Here is the job listing for reference: " +
-            jobListing +
-            ". Please keep the letter professional, engaging, and no more than a few paragraphs long.",
-          
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-
-      const response = await fetch(
-        "https://cover-letter-akira-backend-b62c253075b0.herokuapp.com/completions",  options );
+      const response = await fetch("http://localhost:3001/completions", options);
       const data = await response.json();
-      console.log(data);
-      setCoverLetter(data.choices[0].message);
+      setArticle(data.choices[0].message);
       setIsLoading(false);
     } catch (error) {
-      console.error("Failed to generate cover letter:", error);
+      console.error("Failed to generate article:", error);
       setIsLoading(false);
     }
   };
 
-
-  const handleCopyCoverLetter = async () => {
-    if (coverLetterRef.current) {
-      const el = coverLetterRef.current;
+  const handleCopyArticle = async () => {
+    if (articleRef.current) {
+      const el = articleRef.current;
       try {
         await navigator.clipboard.writeText(el.innerText);
-        // console.log('Text copied to clipboard');
-        alert("Cover Letter Copied to Clipboard!")
+        alert("Article Copied to Clipboard!")
       } catch (error) {
         console.error('Failed to copy text to clipboard:', error);
       }
     }
   };
-  
-  
-  
-  
 
   return (
-<div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center pt-32 md:pt-56">
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center pt-32 md:pt-56">
       <div className="bg-white p-8 rounded-xl shadow-xl w-[80%]">
         <h1 className="text-4xl font-bold text-center ">
-        ⭐ Cover Letter Generator ⭐ 
+          ⭐ Article Creation Assistant ⭐ 
         </h1>
         <h2 className="mb-6 text-lg font-light ">powered by ChatGPT</h2>
-       
+
         <div className="mb-4">
-          <label className="block font-medium mb-2">What is the position?</label>
+          <label className="block font-medium mb-2">What is the main topic of your article?</label>
           <input
             className="input-large bg-gray-200 rounded-lg w-1/2 shadow-sm resize-y"
             type="text"
-            value={position}
-            onChange={(e) => setPosition(e.target.value)}
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
           />
         </div>
         <div className="mb-4">
-          <label className="block font-medium mb-2">Company Name:</label>
+          <label className="block font-medium mb-2">Who are you writing this article for?</label>
           <input
             className="input-large bg-gray-200 rounded-lg shadow-sm w-1/2 resize-y"
             type="text"
-            value={company}
-            onChange={(e) => setCompany(e.target.value)}
+            value={audience}
+            onChange={(e) => setAudience(e.target.value)}
           />
-        </div> 
+        </div>
         <div className="mb-4">
-          <label className="block font-medium mb-2">
-            What are your  skills and/or past experiences relevant to this position? 
-          </label>
+          <label className="block font-medium mb-2">What is the main goal of your article?</label>
+          <input
+            className="input-large bg-gray-200 rounded-lg shadow-sm w-1/2 resize-y"
+            type="text"
+            value={goal}
+            onChange={(e) => setGoal(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">What key takeaway(s) do you want readers to get?</label>
           <input
             className="input-large bg-gray-200 rounded-lg shadow-sm resize-y h-24 w-3/4"
             type="text"
-            value={skills}
-            onChange={(e) => setSkills(e.target.value)}
+            value={takeaways}
+            onChange={(e) => setTakeaways(e.target.value)}
           />
         </div>
         <div className="mb-4">
-          <label className="block font-medium mb-2">What are the company values?</label>
+          <label className="block font-medium mb-2">What is your unique angle or approach (optional)?</label>
           <input
-            className="input-large bg-gray-200 rounded-lg w-3/4 shadow-sm resize-y h-24 "
+            className="input-large bg-gray-200 rounded-lg w-3/4 shadow-sm resize-y h-24"
             type="text"
-            value={companyValues}
-            onChange={(e) => setCompanyValues(e.target.value)}
+            value={angle}
+            onChange={(e) => setAngle(e.target.value)}
           />
         </div>
         <div className="mb-4">
-          <label className="block font-medium  mb-2">
-            Copy and paste job listing here:
-          </label>
-          <textarea
-            className="input-large bg-gray-200 w-3/4 rounded-lg shadow-sm resize-y h-24 "
-            value={jobListing}
-            onChange={(e) => setJobListing(e.target.value)}
+          <label className="block font-medium mb-2">Do you have any keywords in mind?</label>
+          <input
+            className="input-large bg-gray-200 w-3/4 rounded-lg shadow-sm resize-y h-24"
+            type="text"
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
           />
+          <div className="mt-2">
+            <label className="block font-medium mb-2">If no, select one:</label>
+            <select
+              className="input-large bg-gray-200 rounded-lg shadow-sm"
+              value={keywordHelp}
+              onChange={(e) => setKeywordHelp(e.target.value)}
+            >
+              <option value="No">No, I need help with keyword research</option>
+            </select>
+          </div>
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">What format would you like?</label>
+          <select
+            className="input-large bg-gray-200 rounded-lg shadow-sm"
+            value={format}
+            onChange={(e) => setFormat(e.target.value)}
+          >
+            <option value="Listicle">Listicle (e.g., Top 10 Tips)</option>
+            <option value="Step-by-Step Guide">Step-by-Step Guide</option>
+            <option value="In-depth Analysis">In-depth Analysis</option>
+            <option value="Other">Other (Specify)</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">What tone do you want to use?</label>
+          <select
+            className="input-large bg-gray-200 rounded-lg shadow-sm"
+            value={tone}
+            onChange={(e) => setTone(e.target.value)}
+          >
+            <option value="Formal">Formal</option>
+            <option value="Casual">Casual</option>
+            <option value="Humorous">Humorous</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">Do you have any sources you want to reference?</label>
+          <select
+            className="input-large bg-gray-200 rounded-lg shadow-sm"
+            value={sources}
+            onChange={(e) => setSources(e.target.value)}
+          >
+            <option value="No">No</option>
+            <option value="Yes">Yes, will provide links later</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">Is there any specific information you think is crucial for credibility?</label>
+          <select
+            className="input-large bg-gray-200 rounded-lg shadow-sm"
+            value={credibility}
+            onChange={(e) => setCredibility(e.target.value)}
+          >
+            <option value="No">No</option>
+            <option value="Yes">Yes, will provide details later</option>
+          </select>
         </div>
         <button
-          className="bg-blue-500 text-white hover:bg-blue-600 py-2 px-4 rounded-full font-medium text-center  w-3/4"
-          onClick={handleGenerateCoverLetter}
+          className="bg-blue-500 text-white hover:bg-blue-600 py-2 px-4 rounded-full font-medium text-center w-3/4"
+          onClick={handleGenerateArticle}
         >
-          Generate Cover Letter
+          Generate Article
         </button>
         {isLoading && (
           <div className="flex items-center mt-4">
@@ -190,47 +202,42 @@ function MainPage() {
             <p>Loading...</p>
           </div>
         )}
-   <h2 className="text-2xl font-semibold mt-6">Generated Cover Letter:</h2>
-<div className="h-full flex flex-col justify-center items-center">
-  <div className="border p-4 mt-2 rounded-xl shadow-xl w-3/4 flex flex-col justify-center items-center relative">
-    <div className="h-full flex flex-col overflow-y-auto justify-center items-center">
-      <div className="flex-grow p-8">
-        <p ref={coverLetterRef}>{coverLetter.content}</p>
-      </div>
-      <div className="absolute bottom-2 right-2">
-        <button
-          className="bg-green-500 text-white hover:bg-green-600 py-2 px-6  rounded-full font-medium"
-          onClick={handleCopyCoverLetter}
-        >
-          Copy
-        </button>
-      </div>
-      <div className="absolute bottom-2 left-2">
-        <button
-          className="bg-green-500 text-white hover:bg-green-600 py-2 px-6 rounded-full font-medium"
-          onClick={handleGenerateNewCoverLetter}
-        >
-          Generate New
-        </button>
-      </div>
-    </div>
-  </div>
-
-    
- 
-</div>
-
-<p className="text-red-400  pt-8 text-center">Please note that there a rough 750 word character limit on the full response of this form (the total of all fields). 
-          <br></br>
-          Response time is based off of 's ChatGPT network speed. 
-          <br></br>
-          * If the "Generate Cover Letter" button does not return a response, it is because your prompt is too long, or ChatGPT is too busy to perform a request at this time.
-           </p>
+        <h2 className="text-2xl font-semibold mt-6">Generated Article:</h2>
+        <div className="h-full flex flex-col justify-center items-center">
+          <div className="border p-4 mt-2 rounded-xl shadow-xl w-3/4 flex flex-col justify-center items-center relative">
+            <div className="h-full flex flex-col overflow-y-auto justify-center items-center">
+              <div className="flex-grow p-8">
+                <p ref={articleRef}>{article.content}</p>
+              </div>
+              <div className="absolute bottom-2 right-2">
+                <button
+                  className="bg-green-500 text-white hover:bg-green-600 py-2 px-6 rounded-full font-medium"
+                  onClick={handleCopyArticle}
+                >
+                  Copy
+                </button>
+              </div>
+              <div className="absolute bottom-2 left-2">
+                <button
+                  className="bg-green-500 text-white hover:bg-green-600 py-2 px-6 rounded-full font-medium"
+                  onClick={handleGenerateArticle}
+                >
+                  Generate New
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+        <p className="text-red-400 pt-8 text-center">
+          Please note that there is a rough 750-word character limit on the full response of this form (the total of all fields). 
+          <br />
+          Response time is based on OpenAI's ChatGPT network speed. 
+          <br />
+          * If the "Generate Article" button does not return a response, it is because your prompt is too long, or ChatGPT is too busy to perform a request at this time.
+        </p>
+      </div>
     </div>
   );
 }
-  
-  
 
 export default MainPage;
